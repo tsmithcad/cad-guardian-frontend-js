52c99d441f12bdb54a07bd9b28b5c4fa1f05836a
Thomas Divine Smith II <thomasdivinesmith@gmail.com>
2024-08-18 21:07:52 -0400

    Data policy and  cookie policy currently out of scope for MVP.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => (
       When you visit or interact with our sites, services or tools, we or our authorized service providers may use cookies for storing information to help provide you with a better, faster and safer experience and for marketing purposes.
     </Typography> */}
 
-    <Link href="#">Privacy Policy</Link> | <Link href="#">Data Policy</Link> | <Link href="#">Cookie Policy</Link>
+    {/* <Link href="#">Privacy Policy</Link> | <Link href="#">Data Policy</Link> | <Link href="#">Cookie Policy</Link> */}
   </Box>
 );
 
